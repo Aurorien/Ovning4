0d5dac98dc88998f78d8e15d4bc4e5ed210728e4
Anna Eklund <anna.eklund@yahoo.se>
2025-06-12 22:55:08 +0200

    Add Tasks 1, 2, and 3

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -31,14 +31,16 @@ namespace SkalProj_Datastrukturer_Minne
         /// <param name="args"></param>
         static void Main()
         {
-
             while (true)
             {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+
                 Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
                     + "\n1. Examine a List"
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
-                    + "\n4. CheckParenthesis"
+                    //+ "\n4. CheckParenthesis"
                     + "\n0. Exit the application");
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
@@ -61,9 +63,9 @@ namespace SkalProj_Datastrukturer_Minne
                     case '3':
                         ExamineStack();
                         break;
-                    case '4':
-                        CheckParanthesis();
-                        break;
+                    //case '4':
+                    //    CheckParanthesis();
+                    //    break;
                     /*
                      * Extend the menu to include the recursive 
                      * and iterative exercises.
@@ -92,38 +94,336 @@ namespace SkalProj_Datastrukturer_Minne
              * As a default case, tell them to use only + or -
              * Below you can see some inspirational code to begin working.
             */
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            List<string> theList = new List<string>();
+            bool isEditList = true;
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Examine a List");
+            Console.WriteLine("=========================================\n\n");
+            Console.WriteLine("Add and remove strings to the list.\n" +
+                "Prefix the value you want to add to the list with +, or prefix with - to remove it. " +
+                 "Start your input with q if you want to quit to main menu.\n");
 
-            //switch(nav){...}
+            while (isEditList)
+            {
+                Console.Write("Input here: ");
+
+                string input = Console.ReadLine() ?? string.Empty;
+                char nav = input[0];
+                string value = input.Substring(1);
+
+                switch (nav)
+                {
+                    case 'q':
+                        isEditList = false;
+                        break;
+                    case '+':
+                        theList.Add(value);
+                        Console.WriteLine($"\nList count: {theList.Count()}");
+                        Console.WriteLine($"List capacity: {theList.Capacity}");
+                        Console.WriteLine($"Current list content: {string.Join(", ", theList)}\n");
+                        break;
+                    case '-':
+                        bool isSuccess = theList.Remove(value);
+                        Console.WriteLine(isSuccess ?
+                           $"Removed '{value}'." :
+                           $"'{value}' not found in list.");
+                        Console.WriteLine($"\nList count: {theList.Count()}");
+                        Console.WriteLine($"List capacity: {theList.Capacity}");
+                        Console.WriteLine($"Current list content: {string.Join(", ", theList)}\n");
+                        break;
+                    default:
+                        Console.WriteLine("\nStart your input with either + or -, or q if you want to quit to main menu.\n");
+                        break;
+                }
+            }
         }
+
+        // Task 1
+        //
+        // 1. See above.
+        //
+        // 2. The capacity of the list (the size of the underlying array) increases when the
+        // number of items exceeds the current capacity.
+        //
+        // 3. When the capacity increases, it doubles in size.
+        //
+        // 4. When the capacity increases the array get copied into a bigger array.
+        // That is an expensive operation and is balanced by instead only copy when the list
+        // outgrows its capacity.
+        //
+        // 5. No, the capacity does not decrease as the items in the list get removed, even when
+        // the capacity is increased since before. It is because decreasing it requires as expensive
+        // operation as increasing it. Then it is better performance-wise to keep the list current
+        // capacity because it might increase to that again. It is possible to shrink the capacity
+        // by using TrimExcess if really needed.
+        //
+        // 6. It is advantageous to use arrays over lists when the exact size is known beforehand
+        // and it never changes (like days of the week). Arrays are faster and use less memory than
+        // lists. Use lists when there is a need to add/remove, because it is easier to work with
+        // on lists and that is often worth the tiny extra performance cost. It is possible set the
+        // starting capacity to a list to avoid resizing.
 
         /// <summary>
         /// Examines the datastructure Queue
         /// </summary>
+        /// 
+
+
         static void ExamineQueue()
         {
             /*
-             * Loop this method untill the user inputs something to exit to main menue.
+             * Loop this method until the user inputs something to exit to main menue.
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Queue<string> theQueue = new Queue<string>();
+            bool isEditQueue = true;
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Examine a Queue");
+            Console.WriteLine("=========================================\n\n");
+            Console.WriteLine("An interactive queue simulation!\n" +
+                "To add a person to the queue, input + and follow with the name of the person.\n" +
+                 "Input - to make the first person get expedited, satisfied and leave the queue\n" +
+                 "Exit to main menu by inputting q.");
+
+
+            while (isEditQueue)
+            {
+                Console.Write("Input here: ");
+
+                string input = Console.ReadLine() ?? string.Empty;
+                char nav = input[0];
+                string value = input[1..];
+
+                switch (nav)
+                {
+                    case 'q':
+                        isEditQueue = false;
+                        break;
+                    case '+':
+                        theQueue.Enqueue(value);
+                        Console.WriteLine($"\nQueue count: {theQueue.Count()}");
+                        Console.WriteLine($"Current queue content: {string.Join(", ", theQueue)}\n");
+                        break;
+                    case '-':
+                        theQueue.Dequeue();
+                        Console.WriteLine($"\nQueue count: {theQueue.Count()}");
+                        Console.WriteLine($"Current queue content: {string.Join(", ", theQueue)}\n");
+                        break;
+                    default:
+                        Console.WriteLine("\nStart your input with either + or -, or q if you want to quit to main menu.\n");
+                        break;
+                }
+            }
         }
+
+        // Task 2
+        //
+        // 1.
+        //
+        //    a. Starts with an empty cashier queue.
+        //       CashierQueue[]
+        //
+        //    b. Kalle enters the queue.
+        //       Enqueue(Kalle)
+        //       CashierQueue[Kalle]
+        //
+        //    c. Greta enters the queue.
+        //       Enqueue(Greta)
+        //       CashierQueue[Kalle, Greta]
+        //
+        //    d. Kalle get expedited and leaves the queue.
+        //       Dequeue()
+        //       CashierQueue[Greta]
+        //
+        //    e. Stina enters the queue.
+        //       Enqueue(Stina)
+        //       CashierQueue[Greta, Stina]
+        //
+        //    f. Greta get expedited and leaves the queue.
+        //       Dequeue()
+        //       CashierQueue[Stina]
+        //
+        //    g. Olle enters the queue.
+        //       Enqueue(Olle)
+        //       CashierQueue[Stina, Olle]
+        //
+        //    h. Sonny enters the queue.
+        //       Enqueue(Sonny)
+        //       CashierQueue[Stina, Olle, Sonny]
+        //
+        //    i. Stina get expedited and leaves the queue.
+        //       Dequeue()
+        //       CashierQueue[Olle, Sonny]
+        //    ...
+        //
+        // 2. See code implementation above.
+
 
         /// <summary>
         /// Examines the datastructure Stack
         /// </summary>
+        //static void ExamineStack()
+        //{
+        //    /*
+        //     * Loop this method until the user inputs something to exit to main menue.
+        //     * Create a switch with cases to push or pop items
+        //     * Make sure to look at the stack after pushing and and poping to see how it behaves
+        //    */
+
+        //    Console.Clear();
+        //    Console.ForegroundColor = ConsoleColor.Green;
+        //    Stack<string> theStack = new Stack<string>();
+        //    bool isEditStack = true;
+
+        //    Console.WriteLine("=========================================");
+        //    Console.WriteLine("Examine a Stack");
+        //    Console.WriteLine("=========================================\n\n");
+        //    Console.WriteLine("An interactive stack simulation!\n" +
+        //        "To add a person to the stack, input + and follow with the name of the person.\n" +
+        //         "Input - to make the first person get expedited, satisfied and leave the stack\n" +
+        //         "Exit to main menu by inputting q.");
+
+
+        //    while (isEditStack)
+        //    {
+        //        Console.Write("Input here: ");
+
+        //        string input = Console.ReadLine() ?? string.Empty;
+        //        char nav = input[0];
+        //        string value = input[1..];
+
+        //        switch (nav)
+        //        {
+        //            case 'q':
+        //                isEditStack = false;
+        //                break;
+        //            case '+':
+        //                theStack.Push(value);
+        //                Console.WriteLine($"\nQueue count: {theStack.Count()}");
+        //                Console.WriteLine($"Current queue content: {string.Join(", ", theStack)}\n");
+        //                break;
+        //            case '-':
+        //                theStack.Pop();
+        //                Console.WriteLine($"\nQueue count: {theStack.Count()}");
+        //                Console.WriteLine($"Current queue content: {string.Join(", ", theStack)}\n");
+        //                break;
+        //            default:
+        //                Console.WriteLine("\nStart your input with either + or -, or q if you want to quit to main menu.\n");
+        //                break;
+        //        }
+        //    }
+        //}
+
         static void ExamineStack()
         {
-            /*
-             * Loop this method until the user inputs something to exit to main menue.
-             * Create a switch with cases to push or pop items
-             * Make sure to look at the stack after pushing and and poping to see how it behaves
-            */
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            bool isEditStack = true;
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Examine a Stack");
+            Console.WriteLine("=========================================\n\n");
+            Console.WriteLine("An interactive stack simulation!\n" +
+                "To reverse a text, input r followed by the text you want to reverse and press Enter.\n" +
+                 "Exit to main menu by inputting q and pressing Enter.");
+
+
+            while (isEditStack)
+            {
+                Console.Write("\nInput here: ");
+                string input = Console.ReadLine() ?? string.Empty;
+                char nav = input[0];
+                string value = input[1..];
+
+                switch (nav)
+                {
+                    case 'q':
+                        isEditStack = false;
+                        break;
+                    case 'r':
+                        Console.WriteLine($"\nReversed text:\n{ReverseText(value)}\n");
+                        break;
+                    default:
+                        Console.WriteLine("\nInput r followed by a text to reverse, or q if you want to quit to main menu.\n");
+                        break;
+                }
+
+            }
+
         }
+
+        static string ReverseText(string text)
+        {
+            Stack<char> theCharStack = new Stack<char>();
+
+            foreach (char c in text)
+            {
+                theCharStack.Push(c);
+            }
+
+            //StringBuilder sb = new StringBuilder();
+            //while (theCharStack.Count > 0)
+            //{
+            //    sb.Append(theCharStack.Pop());
+            //}
+            //return sb.ToString();
+
+            return new string(theCharStack.ToArray());
+        }
+
+        // Task 3
+        //
+        // 1.
+        //
+        //    a. Starts with an empty cashier queue.
+        //       CashierQueue[]
+        //
+        //    b. Kalle enters the queue.
+        //       Push(Kalle)
+        //       CashierQueue[Kalle]
+        //
+        //    c. Greta enters the queue.
+        //       Push(Greta)
+        //       CashierQueue[Greta, Kalle]
+        //
+        //    d. Greta get expedited and leaves the queue.
+        //       Pop()
+        //       CashierQueue[Kalle]
+        //
+        //    e. Stina enters the queue.
+        //       Push(Stina)
+        //       CashierQueue[Stina, Kalle]
+        //
+        //    f. Stina get expedited and leaves the queue.
+        //       Pop()
+        //       CashierQueue[Kalle]
+        //
+        //    g. Olle enters the queue.
+        //       Push(Olle)
+        //       CashierQueue[Olle, Kalle]
+        //
+        //    h. Sonny enters the queue.
+        //       Push(Sonny)
+        //       CashierQueue[Sonny, Olle, Kalle]
+        //
+        //    i. Sonny get expedited and leaves the queue.
+        //       Pop()
+        //       CashierQueue[Olle, Kalle]
+        //    ...
+        //
+        //    Using Stack for a queue is a bad idea because then the first person entering the queue is the one
+        //    that gets expedited last. Which goes against the cashier queue rules where the first person entering
+        //    the queue should get help first.
+        //
+        // 2. See code implementation above.
 
         static void CheckParanthesis()
         {
