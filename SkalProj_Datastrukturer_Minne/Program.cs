9cec8984947fdceeca4de6d5d59492183838bff1
Anna Eklund <anna.eklund@yahoo.se>
2025-06-12 12:58:22 +0200

    Add answers and descriptions of stack and heap

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// Stack and Heap
+// 1. The stack is a region of memory that stores temporary variables created by each method call,
+//    like local variables and references. E.g. local variables in a method are stored on the stack
+//    and are removed when the method exits. It operates sequentially, in a last-in, first-out (LIFO) manner,
+//    meaning the last item added is the first one to be removed. That is a region of memory used
+//    for dynamic memory allocation, where objects are created and destroyed at runtime. On the heap eveything is
+//    accessible at once through pointers by the handles references. As long as objects on the heap have a reference
+//    they are active and can remain in memory. When they don't have a reference anymore they are put in
+//    garbage collection queue to be cleaned up later by the garbage collector and can not be accessed anymore.
+//    The stack is used for static memory allocation, where the size of the data is known at compile time.
+//    Variables stored on the stack are automatically deallocated when they go out of scope. 
+//
+// 2. Value types are types from System.ValueType and are normally stored directly on the stack.
+//    They can also be stored on the heap when it is a part of a reference-type object.
+//    Reference types are types that inherit from System.Object (or are System.Object.object)
+//    and are stored on the heap.
+//
+// 3. In the first method the variables x and y are of type int which is a value type.
+//    At y = x, the value of x is copied to y, while they are still independent values. 
+//    In the second method, y and x are created as separate references to objects of type MyInt.
+//    At y = x in this method, the x reference is copied to y and then both y and x is pointing to
+//    the same object. So when we change the value of y, it also changes the value of x. 
 
 namespace SkalProj_Datastrukturer_Minne
 {
